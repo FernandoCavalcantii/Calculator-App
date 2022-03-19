37d1f4acc06ce2fe1f1b35b4f289395ddf25c1a4
fernando-cavalcanti <fafhcavalcanti@gmail.com>
2022-03-19 09:30:50 -0300

    Login page and Header

---
+++ calculator-app/src/Context/Login/LoginProvider.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import LoginContext from "./LoginContext";
+
+const LoginProvider = ({ children }) => {
+  const [userName, setUserName] = useState('');
+  const value = {
+    userName,
+    setUserName,
+  }
+  return (
+    <LoginContext.Provider value={ value }>
+      { children }
+    </LoginContext.Provider>
+  );
+}
+
+export default LoginProvider;
--- calculator-app/src/Pages/Calculator/Calculator.js
+++ calculator-app/src/Pages/Calculator/Calculator.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Header from '../../Components/Header';
 
 const Calculator = () => {
+  // const characters  = ['9', '8', 7, 6, 5, 4, 3, 2, 1];
   return (
-    <Header />
+    <>
+      <Header />
+    </>
   )
 };
 
