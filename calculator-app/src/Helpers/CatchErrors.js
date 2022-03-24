74aac8c6c227a33e2e748311cfd100a08b0d9dd5
fernando-cavalcanti <fafhcavalcanti@gmail.com>
2022-03-23 22:05:29 -0300

    Add emojis, calculator css, part of calculator logic and CatchParenthesisError helper

---
+++ calculator-app/src/Helpers/CatchErrors.js
@@ -0,0 +1,17 @@
+export const catchParenthesisError = (expression) => {
+    let openingParenthesis = 0;
+    let closureParenthesis = 0;
+  
+    for (const character of expression) {
+      if (character === '(') {
+        openingParenthesis += 1;
+      }
+      if (character === ')') {
+        closureParenthesis += 1;
+      }
+    }
+    if (openingParenthesis === closureParenthesis) {
+      return false;
+    }
+    return true;
+  }
--- calculator-app/src/Pages/Calculator/Calculator.js
+++ calculator-app/src/Pages/Calculator/Calculator.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../Components/Header';
+import style from './style.module.css';
+import Emoji from '../../Emojis';
+import { catchParenthesisError } from '../../Helpers/CatchErrors';
+<meta charset="UTF-8" />
 
 const Calculator = () => {
-  // const characters  = ['9', '8', 7, 6, 5, 4, 3, 2, 1];
+  const characters  = [7, 8, 9, '/', <Emoji symbol="⟵"/>, <Emoji symbol="⊂"/>, 4, 5, 6, <Emoji symbol="×"/>, '(', ')', 1, 2, 3, '-', <Emoji symbol="x²"/>, <Emoji symbol="√"/>, 0, '.', '%', '+', '='];
+  const [display, setDisplay] = useState('');
+  const [error, setError] = useState(false);
+
+  const calculate = () => {
+    const expression = display.split('');
+    if (catchParenthesisError(expression)) {
+      return setError(true);
+    }
+    if (expression.includes('(') && expression.includes(')')) {
+      const initialParentheses = expression.findIndex((el) => el === '(');
+      const closureParentheses = expression.findIndex((el) => el === ')');
+      console.log(initialParentheses);
+      console.log(closureParentheses);
+      setError(false);
+    }
+    // const result = String(expression.reduce((a, b) => Number(a) + Number(b)));
+    // setDisplay(result);
+  }
+  const handleClick = ({ target }) => {
+    const expression = display.split('');
+    switch(target.innerText) {
+      case "⟵":
+        expression.pop();
+        setDisplay(expression.join(''));
+        break;
+      case "⊂":
+        setDisplay('');
+        break;
+      case "x²":
+        setDisplay(display + '²');
+        break;
+      case "=":
+        calculate();
+        break;
+      default:
+        if (display.includes('.') && target.innerText === '.') {
+          setDisplay(display);
+        } else {
+          setDisplay(display + target.innerText);
+        }
+    }
+  };
   return (
     <>
       <Header />
+      <main className={ style.mainContainer }>
+        <section className={ style.displayContainer }>
+          { display }
+        </section>
+        {error ? <p>Malformed expression</p> : ''}
+        <section className={ style.calculatorContainer }>
+          {characters.map((character, index) => {
+            if (character === '=') {
+              return (
+                <button key={ `button-${index}` } type="button" className="equal" onClick={ handleClick }>{character}</button>
+              )
+            }
+            return (
+              <button key={ `button-${index}` } type="button" onClick={ handleClick }>{character}</button>
+            )
+          })}
+        </section>
+      </main>
     </>
   )
 };
